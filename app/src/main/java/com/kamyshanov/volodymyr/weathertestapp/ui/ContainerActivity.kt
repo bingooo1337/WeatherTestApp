66fbbf39747262a5cb592b432175a834904bf9b0
Volodymyr Kamyshanov <bingooo1337@gmail.com>
2019-04-28 23:42:59 +0300

    Add Container Activity

---
+++ app/src/main/java/com/kamyshanov/volodymyr/weathertestapp/ui/ContainerActivity.kt
@@ -0,0 +1,13 @@
+package com.kamyshanov.volodymyr.weathertestapp.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.kamyshanov.volodymyr.weathertestapp.R
+
+class ContainerActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_container)
+  }
+}
